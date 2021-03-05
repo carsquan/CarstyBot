b3a016d1c103681bafd4abf41d6f024d9972602c
Carsten Schaufert <70521348+carsquan@users.noreply.github.com>
2021-03-05 18:55:09 -0500

    Update rps.py

--- cogs/rps.py
+++ cogs/rps.py
@@ -1,7 +1,16 @@
 import discord
+from discord import message
 from discord.ext import commands
 
 import random
+
+async def checkWinner(p1,p2):
+    if(p1 == p2):
+        return 0
+    elif ((p1 == 0 and p2 == 1) or (p1 == 1 and p2 == 2 ) or (p1 == 2 and p2 == 0)):
+        return 2
+    elif ((p1 == 1 and p2 == 0) or (p1==2 and p2 == 2) or (p1 ==0 and p2 == 0)):
+        return 0
 
 async def confirmation(self, message, sentUser, channel, successMessage = ':white_check_mark: Operation successful', cancelledMessage = ':x: Operation cancelled'):
     new_message = await channel.send(message)
@@ -32,18 +41,64 @@ async def confirmation(self, message, sentUser, channel, successMessage = ':whit
             await new_message.edit(content=cancelledMessage)
             return False
 
+async def rpsChoice(self, sentUser):
+    
+    message = "Whats your choice"
+    new_message = await sentUser.send(message)
+
+    await new_message.add_reaction('🔥')
+    await new_message.add_reaction('💧')
+    await new_message.add_reaction('🌲')
+
+    def check(reaction, user):
+        return user == sentUser
+
+    reaction = None
+
+    while True:
+        if str(reaction) == '🔥':
+            await sentUser.send(content="You have chosen fire")
+            return 0
+        elif str(reaction) == '💧':
+            await sentUser.send(content="You have chosen water")
+            return 1
+        elif str(reaction) == '🌲':
+            await sentUser.send(content="You have chosen grass")
+            return 2
+
+        try:
+            reaction, user = await self.client.wait_for('reaction_add', timeout = 30.0, check = check)
+            #await new_message.remove_reaction(reaction, user)
+        except:
+            #await new_message.clear_reactions()
+            await sentUser.send(content="You have run out of time(sad)")
+            return
+
+
+
 async def messageUser(self, message, reciever):
     try:
-        dm = discord.DMChannel()
-        dm.recipient = reciever
 
-        return await dm.send(message)
+        return await reciever.send(message)
     except:
         print("ERROR in messageUser\n")
+
+
 async def playRockPaperScissors(self, p1, p2,channel):
-    if not await confirmation(self, f'<@!{p2.id}>! Do you want to play Rock Paper Scissors with {p1.display_name}?', p2, channel, cancelledMessage=':x: Rock Paper Scissors cancelled!', successMessage='Starting Rock Paper Scissors'):
-        return
-    
+    try:
+        if not await confirmation(self, f'<@!{p2.id}>! Do you want to play Rock Paper Scissors with {p1.display_name}?', p2, channel, cancelledMessage=':x: Rock Paper Scissors cancelled!', successMessage='Starting Rock Paper Scissors'):
+            return
+        print("Confirmed")
+        p1Choice = await rpsChoice(self,sentUser = p1)
+        p2Choice = await rpsChoice(self,sentUser = p2)
+        switcher = {
+            0: "Draw",
+            1: f"{p1.display_name} won!",
+            2: f"{p2.display_name} won!",
+        }
+        msg = switcher.get(await checkWinner(p1Choice,p2Choice),"Error or Someone times out smh")
+        return await channel.send(msg)
+    except Exception as e: print(e)
 
 
 class rps(commands.Cog):
@@ -61,10 +116,13 @@ class rps(commands.Cog):
     @commands.command()
     async def rps(self, ctx, user:str):
         #Rock Paper Scissors
+
         print(f'{ctx.author} has challenged {user} to Rock Paper Scissors')
         try:
-            playerTwo = await ctx.guild.fetch_member(int(user[3:-1]))
-
+            p2id = int(user[3:-1])
+            print(p2id)
+            playerTwo = await ctx.guild.fetch_member(p2id)
+            
             await playRockPaperScissors(self, ctx.author, playerTwo, ctx.channel)
         except:
             await ctx.send(':no_entry: Player not found!')
